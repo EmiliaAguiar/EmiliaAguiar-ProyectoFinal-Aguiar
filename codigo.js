514f9e292a3f69ed1146efa36d81b377779b7bd9
Emilia Aguiar <aguiaremilia.ag@gmail.com>
2023-09-20 10:47:25 -0300

    Agregando eventos a boton de comprar y funcion de Agregar al carrito

--- codigo.js
+++ codigo.js
@@ -1,6 +1,7 @@
 console.dir(document.body);
+console.table(platos);
 
-// Aplicando métodos de acceso y modificación de nodos
+// Métodos de acceso y modificación de nodos
 
 // Sección Principal
 
@@ -31,7 +32,7 @@ const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', '
 let tituloH1 = document.getElementById('titulo');
 console.dir(tituloH1);
 let nombreDia = hoy === 0 ? dias[6] : dias[hoy];
-tituloH1.innerText = `Menú del ${nombreDia}`;
+tituloH1.innerText = `Sugerencias del ${nombreDia}`;
 tituloH1.style.font = 'bold 40px Montserrat';
 tituloH1.style.color = '#D6FFE7';
 
@@ -52,93 +53,15 @@ cuentaRegresiva.style.color = '#428C60';
 cuentaRegresiva.style.background = '#E5E2C8';
 cuentaRegresiva.style.borderRadius = '15px';
 
-let sectionPlatos = document.getElementById('menuPlatos')
+let sectionCarrito = document.getElementById('carrito');
+sectionCarrito.style.background = '#D6FFE7';
+sectionCarrito.classList.add('mx-auto', 'p-5');
+sectionCarrito.classList.add('text-center');
+
+let sectionPlatos = document.getElementById('menuPlatos');
 console.dir(sectionPlatos);
 sectionPlatos.style.background = '#E5E2C8';
 
-// Cuenta regresiva del menú del día
-function calcularTiempoRestante() {
-    const ahora = new Date();
-    const horaActual = ahora.getHours();
-    const minutosActual = ahora.getMinutes();
-    const segundosActual = ahora.getSeconds();
-
-    const horaInicio = 11;
-    const horaFin = 14;
-
-    // Fuera del horario del menú
-    if (horaActual >= horaInicio && horaActual < horaFin) {
-        const tiempoRestanteHoras = horaFin - horaActual - 1;
-        const tiempoRestanteMinutos = 59 - minutosActual;
-        const tiempoRestanteSegundos = 59 - segundosActual;
-
-        return {
-            dentroHorario: true,
-            tiempoRestante: {
-                horas: tiempoRestanteHoras,
-                minutos: tiempoRestanteMinutos,
-                segundos: tiempoRestanteSegundos
-            }
-        };
-        // Fuera del horario del menú
-    } else {
-        const manana = new Date(ahora);
-        manana.setDate(manana.getDate() + 1);
-        manana.setHours(horaInicio, 0, 0);
-        const tiempoRestante = Math.floor((manana - ahora) / 1000);
-
-        const horasRestantes = Math.floor(tiempoRestante / 3600);
-        const minutosRestantes = Math.floor((tiempoRestante % 3600) / 60);
-        const segundosRestantes = tiempoRestante % 60;
-
-        return {
-            dentroHorario: false,
-            tiempoRestante: {
-                horas: horasRestantes,
-                minutos: minutosRestantes,
-                segundos: segundosRestantes
-            }
-        };
-    }
-}
-
-function mostrarMensaje() {
-    const resultado = calcularTiempoRestante();
-
-    if (resultado.dentroHorario) {
-        const tiempoRestante = resultado.tiempoRestante;
-        document.getElementById('cuentaRegresiva').innerText = 'Estás a tiempo de hacer tu pedido';
-    } else {
-        document.getElementById('cuentaRegresiva').innerText = `Faltan ${tiempoRestante.horas} horas, ${tiempoRestante.minutos} minutos y ${tiempoRestante.segundos} segundos para el próximo menú del día`;
-    }
-}
-
-mostrarMensaje();
-setInterval(mostrarMensaje, 1000);
-
-
-
-// Array de objetos - Platos
-
-const platos = [
-    { id: 1, nombre: "Arroz al azafrán con pollo", precio: 2350, tipoDeMenu: "Omnívoro", imagen: "./images/arrozConPollo.png" },
-    { id: 2, nombre: "Milanesas de berenjena con puré mixto", precio: 1730, tipoDeMenu: "Vegetariano", imagen: "./images/milaBerenjena.png" },
-    { id: 3, nombre: "Guiso de lentejas", precio: 1770, tipoDeMenu: "Sin Tacc, Omnivoro", imagen: "./images/guiso.png" },
-    { id: 4, nombre: "Tallarines de espinaca con salsa blanca", precio: 2860, tipoDeMenu: "Vegetariano", imagen: "./images/tallarines.png" },
-    { id: 6, nombre: "Pollo al verdeo con papas noissete", precio: 3780, tipoDeMenu: "Omnívoro", imagen: "./images/polloAlVerdeo.png" },
-    { id: 7, nombre: "Sopa crema de calabaza", precio: 1620, tipoDeMenu: "Omnívoro, Sin Tacc, Vegetariano", imagen: "./images/sopa.png" },
-    { id: 8, nombre: "Sorrentinos de calabaza al pesto sin Tacc", precio: 3350, tipoDeMenu: "Sin Tacc, Vegetariano, Omnivoro", imagen: "./images/sorrentinos.png" },
-    { id: 9, nombre: "Sándwich de lomito, queso, lechuga y tomate", precio: 2160, tipoDeMenu: "omnívoro", imagen: "./images/sandwich.png" },
-    { id: 10, nombre: "Tarta individual cremosa de choclo", precio: 2120, tipoDeMenu: "Vegetariano", imagen: "./images/tartita.png" },
-    { id: 11, nombre: "Wrap integral de atun, queso y tomate", precio: 2970, tipoDeMenu: "Omnivoro", imagen: "./images/wrap.png" },
-    { id: 12, nombre: "Bife a la criolla con verduras asadas", precio: 4410, tipoDeMenu: "Omnívoro, Sin Tacc", imagen: "./images/bifes.png" },
-    { id: 13, nombre: "Calabaza rellena de verduras y gratinada", precio: 3850, tipoDeMenu: "Vegetariano, Sin Tacc", imagen: "./images/calabaza.png" },
-    { id: 14, nombre: "Pollo al horno con batatas y ensalada", precio: 4180, tipoDeMenu: "Omnívoro, Sin Tacc", imagen: "./images/polloAlHorno.png" },
-    { id: 15, nombre: "Falafel en pan pita con salsa tahini", precio: 2990, tipoDeMenu: "Vegetariano, Sin Tacc", imagen: "./images/falafel.png" }
-
-]
-
-// Cartas 
 
 let articuloCartas = document.getElementById('cartas');
 articuloCartas.classList.add('container');
@@ -146,23 +69,106 @@ articuloCartas.classList.add('gap-3');
 articuloCartas.classList.add('mx-auto');
 articuloCartas.classList.add('my-3');
 
-for (const plato of platos) {
-    articuloCartas.innerHTML +=
-        `
+// Cuenta Regresiva
+
+function calcularTiempoRestante() {
+    const ahora = new Date();
+    const horaActual = ahora.getHours();
+
+    const horaInicio = 11;
+    const horaFin = 14;
+
+    if (horaActual >= horaInicio && horaActual < horaFin) {
+        return {
+            dentroHorario: true,
+            mensaje: 'Estás a tiempo de hacer tu pedido'
+        };
+    } else if (horaActual >= 0 && horaActual < 11) {
+        return {
+            dentroHorario: false,
+            mensaje: `Falta poco para que puedas hacer tu pedido del ${nombreDia}`
+        };
+    } else {
+        const manana = new Date(ahora);
+        manana.setDate(manana.getDate() + 1);
+        manana.setHours(horaInicio, 0, 0);
+
+        const tiempoRestante = manana - ahora;
+        return {
+            dentroHorario: false,
+            mensaje: `Faltan ${Math.floor(tiempoRestante / 3600000)} horas y ${Math.floor((tiempoRestante % 3600000) / 60000)} minutos para el próximo menú del día`
+        };
+    }
+}
+
+function mostrarMensaje() {
+    const resultado = calcularTiempoRestante();
+
+    document.getElementById('cuentaRegresiva').innerText = resultado.mensaje;
+}
+
+mostrarMensaje();
+setInterval(mostrarMensaje, 1000);
+
+// Cartas de platos
+
+// Cartas de platos
+
+function renderizarPlato(plato) {
+    return `
         <div class="card mb-3" style="max-width: 540px;">
             <div class="row g-0">
                 <div class="col-md-4 d-flex align-items-center">
-                <img src="${plato.imagen}" alt="${plato.nombre} width="150px" class="img-fluid rounded-start">
+                    <img src="${plato.imagen}" alt="${plato.nombre}" width="150px" class="img-fluid rounded-start">
                 </div>
                 <div class="col-md-8 d-flex align-items-center">
                     <div class="card-body">
                         <h5 class="card-title" style="font-size: 20px; color: #428C60; font-family: Montserrat;">${plato.nombre}</h5>
                         <button class="btn btn-secondary" style="background-color:#E8E8E8; border:0px; color:#428C60; font-weight:500; font-size:12px;"> 🥣 Este menú es: ${plato.tipoDeMenu}</button>
                         <p style="font-size: 25px; color: green; font-weight: bold; font-family: Patrick Hand;">$${plato.precio}</p>
-                        <button class="btn btn-success" style="background-color:#428C60; color:#D6FFE7; border:0px; font-family: Montserrat; text-transform: uppercase;"> Comprar </button>
+                        <button id="${plato.id}" class="btn btn-success comprar" style="background-color:#428C60; color:#D6FFE7; border:0px; font-family: Montserrat; text-transform: uppercase;"> Comprar </button>
                     </div>
                 </div>
             </div>
         </div>
-        `;
+    `;
 }
+
+let carrito = []; 
+
+// Evento clic a los botones de compra
+
+function agregarEventoClic() {
+    let botones = document.getElementsByClassName('comprar');
+
+    for (const boton of botones) {
+        boton.addEventListener('click', () => {
+            const prodACarro = platos.find((plato) => plato.id == boton.id);
+            agregarAlCarrito(prodACarro);
+        });
+    }
+}
+
+// Agregar platos al carrito
+
+const contenedorPlatos = document.getElementById('menuPlatos');
+const tablaBody = document.getElementById('tablabody');
+
+
+function agregarAlCarrito(plato) {
+    carrito.push(plato);
+    console.table(carrito);
+    tablaBody.innerHTML += `
+        <tr>
+            <td style="font-size: 16px; color: grey; font-family: Montserrat;">${plato.id}</td>
+            <td style="font-size: 16px; color: grey; font-family: Montserrat;">${plato.nombre}</td>
+            <td style="font-size: 16px; color: grey; font-family: Montserrat;">$${plato.precio}</td>
+        </tr>
+    `;
+}
+
+for (const plato of platos) {
+    articuloCartas.innerHTML += renderizarPlato(plato);
+}
+agregarEventoClic();
+
---
+++ platos.js
@@ -0,0 +1,20 @@
+// Array de objetos - Platos
+
+const platos = [
+    { id: 1, nombre: "Arroz al azafrán con pollo", precio: 2350, tipoDeMenu: "Omnívoro", imagen: "./images/arrozConPollo.png" },
+    { id: 2, nombre: "Milanesas de berenjena con puré mixto", precio: 1730, tipoDeMenu: "Vegetariano", imagen: "./images/milaBerenjena.png" },
+    { id: 3, nombre: "Guiso de lentejas", precio: 1770, tipoDeMenu: "Sin Tacc, Omnivoro", imagen: "./images/guiso.png" },
+    { id: 4, nombre: "Tallarines de espinaca con salsa blanca", precio: 2860, tipoDeMenu: "Vegetariano", imagen: "./images/tallarines.png" },
+    { id: 6, nombre: "Pollo al verdeo con papas noissete", precio: 3780, tipoDeMenu: "Omnívoro", imagen: "./images/polloAlVerdeo.png" },
+    { id: 7, nombre: "Sopa crema de calabaza", precio: 1620, tipoDeMenu: "Sin Tacc, Vegetariano", imagen: "./images/sopa.png" },
+    { id: 8, nombre: "Sorrentinos de calabaza al pesto sin Tacc", precio: 3350, tipoDeMenu: "Sin Tacc, Vegetariano", imagen: "./images/sorrentinos.png" },
+    { id: 9, nombre: "Sándwich de lomito, queso, lechuga y tomate", precio: 2160, tipoDeMenu: "omnívoro", imagen: "./images/sandwich.png" },
+    { id: 10, nombre: "Tarta individual cremosa de choclo", precio: 2120, tipoDeMenu: "Vegetariano", imagen: "./images/tartita.png" },
+    { id: 11, nombre: "Wrap integral de atun, queso y tomate", precio: 2970, tipoDeMenu: "Omnivoro", imagen: "./images/wrap.png" },
+    { id: 12, nombre: "Bife a la criolla con verduras asadas", precio: 4410, tipoDeMenu: "Omnívoro, Sin Tacc", imagen: "./images/bifes.png" },
+    { id: 13, nombre: "Calabaza rellena de verduras al graten", precio: 3850, tipoDeMenu: "Vegetariano, Sin Tacc", imagen: "./images/calabaza.png" },
+    { id: 14, nombre: "Pollo al horno con batatas y papas", precio: 4180, tipoDeMenu: "Omnívoro, Sin Tacc", imagen: "./images/polloAlHorno.png" },
+    { id: 15, nombre: "Falafel en pan pita con salsa tahini", precio: 2990, tipoDeMenu: "Vegetariano, Sin Tacc", imagen: "./images/falafel.png" }
+
+]
+
